d2b21bda2a96557c707b999707a4e01145f5b9a9
Aysu <aysu@Noutbuk-Aysu.local>
2025-02-10 16:40:56 +0400

    chages in json file

--- movie-project/vite.config.js
+++ movie-project/vite.config.js
@@ -2,4 +2,15 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        favorites: "favorites.html",
+        edit: "edit.html",
+        detail: "detail.html",
+        add: "add.html",
+      },
+    },
+  },
 });
